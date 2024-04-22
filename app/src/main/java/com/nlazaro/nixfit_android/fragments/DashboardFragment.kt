a99221d30f1a93c5f08686d93439a44de6c449df
Nixon L <nicklazaro14@gmail.com>
2024-04-22 17:12:21 -0400

    more cleanup. base template.

--- app/src/main/java/com/nlazaro/nixfit_android/fragments/DashboardFragment.kt
+++ app/src/main/java/com/nlazaro/nixfit_android/fragments/DashboardFragment.kt
@@ -1,24 +1,22 @@
-package com.nlazaro.nixfit_android.ui.home
+package com.nlazaro.nixfit_android.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.nlazaro.nixfit_android.R
-import com.nlazaro.nixfit_android.databinding.FragmentHomeBinding
+import com.nlazaro.nixfit_android.databinding.FragmentDashboardBinding
 
-class HomeFragment : Fragment() {
+class DashboardFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflates the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
