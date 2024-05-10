bc2f6f5aae99b2f8efdce976ed651ffe77cb332a
Nixon L <nicklazaro14@gmail.com>
2024-05-10 16:53:29 -0400

    fixed di problem

--- app/src/main/java/com/example/nixfit/MainActivity.kt
+++ app/src/main/java/com/example/nixfit/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
--- app/src/main/java/com/example/nixfit/NixFitApplication.kt
+++ app/src/main/java/com/example/nixfit/NixFitApplication.kt
@@ -1,9 +1,6 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
 package com.example.nixfit
 
 import android.app.Application
-import androidx.annotation.StringRes
-import androidx.compose.material3.ExperimentalMaterial3Api
 import dagger.hilt.android.HiltAndroidApp
 
 
